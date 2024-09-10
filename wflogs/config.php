5cf141a68022c4f427dc69bdd373c12c8290a0ba
Ardayann <132604592+Ardayann@users.noreply.github.com>
2024-09-10 02:04:12 -0500

    Add files via upload

---
+++ wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1726547348;s:7:"authKey";s:64:"bIR^qG23XA.VGoxdJqXJLm.T*eR^xe bH)pN8`[LU#Tf/.^-ZT3AO3v^]TP#mmVB";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1679;}
